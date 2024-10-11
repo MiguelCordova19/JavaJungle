5331d1d6a0ef6aa2a3b189962f906674e66ef784
LAB-USR-CHIMBOTE <LAB-USR-CHIMBOTE@grupoutp.net>
2024-10-11 16:21:59 -0500

    Subi clase Vehiculo

--- GestionDeVehiculos/src/Vehiculo.java
+++ GestionDeVehiculos/src/Vehiculo.java
@@ -1,12 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author LAB-USR-CHIMBOTE
- */
 public class Vehiculo {
-    
+    private String marca;
+    private String modelo;
+
+    public Vehiculo(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public String mostrarInfo() {
+        return "Marca: " + getMarca() + ", Modelo: " + getModelo();
+    }
+
+    /**
+     * @return the marca
+     */
+    public String getMarca() {
+        return marca;
+    }
+
+    /**
+     * @param marca the marca to set
+     */
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    /**
+     * @return the modelo
+     */
+    public String getModelo() {
+        return modelo;
+    }
+
+    /**
+     * @param modelo the modelo to set
+     */
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
 }
